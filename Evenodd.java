6544bc9eff1321ce8761f0b829c362a034a24710
vempati-vijay-kumar-reddy <164998970+vempati-vijay-kumar-reddy@users.noreply.github.com>
2024-07-29 14:21:26 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.evenodd;
+
+/**
+ *
+ * @author vempa
+ */
+public class Evenodd {
+
+    public static void main(String[] args) {
+        int num=16;
+        if (num%2==0){
+    System.out.println(num+"is even number");
+    } 
+        else
+    {
+    System.out.println(num+ "is not even number");
+           
+    }
+}
+}
